5ec60466ec1023ae55fffbd6177c2e7a3e96acd3
lautaro.corceiro <lautaro.corceiro@megatrans.com.ar>
2025-09-17 16:25:12 -0300

    menu dinamico

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("proyecto");
+const links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("main section");
+
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+
+    // ocultar todas
+    sections.forEach(sec => sec.classList.remove("active"));
+
+    // mostrar la elegida
+    document.getElementById(targetId).classList.add("active");
+
+    // actualizar link activo
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
